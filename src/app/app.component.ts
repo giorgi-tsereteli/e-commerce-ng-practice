d2ada5c2c775a3ee9e5582dcf63fc6d5e6b294cd
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-02-27 16:09:48 -0800

    Create new project and add material modules

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello world</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'store';
+}
