ac57b389df52ce5026a5e8a59fc2ce43d3304f47
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-03-14 21:01:14 -0700

    Add functionality to increase or decrease item quantity in cart

--- src/app/pages/home/cart/cart.component.ts
+++ src/app/pages/home/cart/cart.component.ts
@@ -26,6 +26,7 @@ export class CartComponent implements OnInit {
     ],
   };
 
+  // Return to this part and review why dataSource was added
   dataSoure: Array<CartItem> = [];
 
   displayedColumns: Array<string> = [
@@ -40,10 +41,40 @@ export class CartComponent implements OnInit {
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.dataSoure = this.cart.items;
+    // Subscribe to cart changes to update the cart items
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSoure = this.cart.items;
+    });
   }
 
   getTotal(items: Array<CartItem>): number {
     return this.cartService.getTotal(items);
   }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(productId: number): void {
+    console.log(productId);
+    this.cartService.removeFromCart(productId);
+  }
+
+  onRemoveFromCart2(item: CartItem): void {
+    this.cartService.removeFromCart2(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item)
+    // Initially u made another func in service to add quantity
+    // The logic of += 1 is already within addToCart method in the service class
+    // this.cartService.addQuantity(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,6 +7,7 @@ import { MatSnackBar } from "@angular/material/snack-bar";
   providedIn: "root",
 })
 export class CartService {
+  // BehaviorSubject is a type of Subject, a special type of Observable that allows you to multicast to many Observers.
   cart = new BehaviorSubject<Cart>({ items: [] });
 
   constructor(private _snackBar: MatSnackBar) {}
@@ -26,7 +27,7 @@ export class CartService {
 
     // By calling next with {items}, you are updating the state of the cart with the new list of items.
     this.cart.next({ items });
-    this._snackBar.open("1 Item addeded to cart", "Ok", { duration: 2000 });
+    this._snackBar.open("1 Item addeded to cart", "Ok", { duration: 1000 });
     console.log(this.cart.value);
   }
 
@@ -38,6 +39,36 @@ export class CartService {
 
   clearCart(): void {
     this.cart.next({ items: [] });
-    this._snackBar.open("Cart cleared", "Ok", { duration: 2000 });
+    this._snackBar.open("Cart cleared", "Ok", { duration: 1000 });
+  }
+
+  removeFromCart(productId: number): void {
+    const items = this.cart.value.items.filter((item) => item.id !== productId);
+    this.cart.next({ items });
+    this._snackBar.open("Item removed from cart", "Ok", { duration: 1000 });
+  }
+
+  removeFromCart2(item: CartItem): void {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open("Item removed from cart", "Ok", { duration: 1000 });
+  }
+
+  removeQuantity(item: CartItem): void {
+    const items = [...this.cart.value.items];
+    const itemInCart = items.find((_item) => _item.id === item.id);
+    if (itemInCart) {
+      itemInCart.quantity -= 1;
+      this._snackBar.open("1 item removed from cart", "Ok", { duration: 500 });
+      if (itemInCart.quantity === 0) {
+        this.removeFromCart2(item); // method to fully clear the cart
+      } else {
+        this.cart.next({ items });
+      }
+    } else {
+      console.log("Item not found in cart");
+    }
   }
 }
