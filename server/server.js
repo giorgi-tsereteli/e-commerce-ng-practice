7949854062a039a72dd8bceee89f573596e58aea
giorgi-tsereteli <g.tsereteli@elsevier.com>
2025-03-22 17:40:25 -0700

    Add shipping options to checkout function

--- server/server.js
+++ server/server.js
@@ -12,9 +12,58 @@ const stripe = require("stripe")(
   "sk_test_51R36qeHhN0WnAkU4wnSNCKO7TnQCwP8nO19t3x1CkGe7LCBTUrnYyKtHD4uQWHlmDwwDWyvhjkzm3l5dzXCfHxqE003bfwcj3T"
 );
 
+
+// https://docs.stripe.com/payments/during-payment/charge-shipping
 app.post("/checkout", async (req, res, next) => {
   try {
     const session = await stripe.checkout.sessions.create({
+      billing_address_collection: "required",
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
+            },
+            display_name: "Free shipping",
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+
       line_items: req.body.items.map((item) => ({
         price_data: {
           currency: "usd",
